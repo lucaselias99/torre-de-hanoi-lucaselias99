150d2b560052f1379f15133281e05986d6038b84
rafaeldev <elderrafaelgomes@gmail.com>
2021-08-03 00:45:52 -0300

    Correções na função de movimento dos discos!

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,96 +58,98 @@ function createTower() {
 }
 
 createTower();
-
+clickUm = false;
+clickDois = false;
+clickTres = false;
 torreUm.addEventListener('click', function(e) {
-    clickum = true;
+    clickUm = true;
     //console.log(clickum)
     //console.log(e.currentTarget)
-    if(clickdois===true) {
+    if(clickDois===true) {
         if(torreUm.lastElementChild === null){
             torreUm.appendChild(torreDois.lastElementChild);
-            clickdois = false;
-            clickum = false;
+            clickDois = false;
+            clickUm = false;
         } else if(torreUm.lastElementChild != null){
             if(torreDois.lastElementChild.clientWidth < torreUm.lastElementChild.clientWidth ){
                 torreUm.appendChild(torreDois.lastElementChild);
-                clickdois = false;
-                clickum = false;
+                clickDois = false;
+                clickUm = false;
             }
         }
     }
-    if(clicktres===true) {
+    if(clickTres===true) {
         if(torreUm.lastElementChild === null){
             torreUm.appendChild(torreTres.lastElementChild);
-            clicktres = false;
-            clickum = false;
+            clickTres = false;
+            clickUm = false;
         } else if(torreUm.lastElementChild != null){
             if(torreTres.lastElementChild.clientWidth < torreUm.lastElementChild.clientWidth ){
                 torreUm.appendChild(torreTres.lastElementChild);
-                clicktres = false;
-                clickum = false;
+                clickTres = false;
+                clickUm = false;
             }
         }
     }
 })
 torreDois.addEventListener('click',function(e) {
-    clickdois = true;
+    clickDois = true;
     //console.log(clickdois)
     //console.log(e.currentTarget)
     if(clickUm===true) {
         if(torreDois.lastElementChild === null){
             torreDois.appendChild(torreUm.lastElementChild);
-            clickum = false;
-            clickdois = false;
+            clickUm = false;
+            clickDois = false;
         } else if(torreDois.lastElementChild != null){
             if(torreUm.lastElementChild.clientWidth < torreDois.lastElementChild.clientWidth ){
                 torreDois.appendChild(torreUm.lastElementChild);
-                clickum = false;
-                clickdois = false;
+                clickUm = false;
+                clickDois = false;
             }
         }
     }
-    if(clicktres===true) {
+    if(clickTres===true) {
         if(torreDois.lastElementChild === null){
             torreDois.appendChild(torreTres.lastElementChild);
-            clicktres = false;
-            clickdois = false;
+            clickTres = false;
+            clickDois = false;
         } else if(torreDois.lastElementChild != null){
             if(torreTres.lastElementChild.clientWidth < torreDois.lastElementChild.clientWidth ){
                 torreDois.appendChild(torreTres.lastElementChild);
-                clicktres = false;
-                clickdois = false;
+                clickTres = false;
+                clickdDois = false;
             }
         }
     }
 })
 torreTres.addEventListener('click',function(e) {
-    clicktres = true;
+    clickTres = true;
     //console.log(clicktres)
     //console.log(e.currentTarget)
-    if(clickum===true) {
+    if(clickUm===true) {
         if(torreTres.lastElementChild === null){
             torreTres.appendChild(torreUm.lastElementChild);
-            clickum = false;
-            clicktres = false;
+            clickUm = false;
+            clickTres = false;
         } else if(torreTres.lastElementChild != null){
             if(torreUm.lastElementChild.clientWidth < torreTres.lastElementChild.clientWidth ){
                 torreTres.appendChild(torreUm.lastElementChild);
-                clickum = false;
-                clicktres = false;
+                clickUm = false;
+                clickTres = false;
             }
         }
     }
-    if(clickdois===true) {
+    if(clickDois===true) {
         if(torreTres.lastElementChild === null){
             torreTres.appendChild(torreDois.lastElementChild);
-            clickdois = false;
-            clicktres = false;
+            clickDois = false;
+            clickTres = false;
         } else if(torreTres.lastElementChild != null){
             if(torreDois.lastElementChild.clientWidth < torreTres.lastElementChild.clientWidth ){
                 torreTres.appendChild(torreDois.lastElementChild);
-                clickdois = false;
-                clicktres = false;
+                clickDois = false;
+                clickTres = false;
             }
         }
     }
