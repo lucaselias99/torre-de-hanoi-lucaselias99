d519ae84e91db5d3b917c8a4973a8a2aa437a820
rafaeldev <elderrafaelgomes@gmail.com>
2021-08-03 00:33:43 -0300

    Criada função de movimento de discos!

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,4 +59,96 @@ function createTower() {
 
 createTower();
 
-
+torreUm.addEventListener('click', function(e) {
+    clickum = true;
+    //console.log(clickum)
+    //console.log(e.currentTarget)
+    if(clickdois===true) {
+        if(torreUm.lastElementChild === null){
+            torreUm.appendChild(torreDois.lastElementChild);
+            clickdois = false;
+            clickum = false;
+        } else if(torreUm.lastElementChild != null){
+            if(torreDois.lastElementChild.clientWidth < torreUm.lastElementChild.clientWidth ){
+                torreUm.appendChild(torreDois.lastElementChild);
+                clickdois = false;
+                clickum = false;
+            }
+        }
+    }
+    if(clicktres===true) {
+        if(torreUm.lastElementChild === null){
+            torreUm.appendChild(torreTres.lastElementChild);
+            clicktres = false;
+            clickum = false;
+        } else if(torreUm.lastElementChild != null){
+            if(torreTres.lastElementChild.clientWidth < torreUm.lastElementChild.clientWidth ){
+                torreUm.appendChild(torreTres.lastElementChild);
+                clicktres = false;
+                clickum = false;
+            }
+        }
+    }
+})
+torreDois.addEventListener('click',function(e) {
+    clickdois = true;
+    //console.log(clickdois)
+    //console.log(e.currentTarget)
+    if(clickUm===true) {
+        if(torreDois.lastElementChild === null){
+            torreDois.appendChild(torreUm.lastElementChild);
+            clickum = false;
+            clickdois = false;
+        } else if(torreDois.lastElementChild != null){
+            if(torreUm.lastElementChild.clientWidth < torreDois.lastElementChild.clientWidth ){
+                torreDois.appendChild(torreUm.lastElementChild);
+                clickum = false;
+                clickdois = false;
+            }
+        }
+    }
+    if(clicktres===true) {
+        if(torreDois.lastElementChild === null){
+            torreDois.appendChild(torreTres.lastElementChild);
+            clicktres = false;
+            clickdois = false;
+        } else if(torreDois.lastElementChild != null){
+            if(torreTres.lastElementChild.clientWidth < torreDois.lastElementChild.clientWidth ){
+                torreDois.appendChild(torreTres.lastElementChild);
+                clicktres = false;
+                clickdois = false;
+            }
+        }
+    }
+})
+torreTres.addEventListener('click',function(e) {
+    clicktres = true;
+    //console.log(clicktres)
+    //console.log(e.currentTarget)
+    if(clickum===true) {
+        if(torreTres.lastElementChild === null){
+            torreTres.appendChild(torreUm.lastElementChild);
+            clickum = false;
+            clicktres = false;
+        } else if(torreTres.lastElementChild != null){
+            if(torreUm.lastElementChild.clientWidth < torreTres.lastElementChild.clientWidth ){
+                torreTres.appendChild(torreUm.lastElementChild);
+                clickum = false;
+                clicktres = false;
+            }
+        }
+    }
+    if(clickdois===true) {
+        if(torreTres.lastElementChild === null){
+            torreTres.appendChild(torreDois.lastElementChild);
+            clickdois = false;
+            clicktres = false;
+        } else if(torreTres.lastElementChild != null){
+            if(torreDois.lastElementChild.clientWidth < torreTres.lastElementChild.clientWidth ){
+                torreTres.appendChild(torreDois.lastElementChild);
+                clickdois = false;
+                clicktres = false;
+            }
+        }
+    }
+})
